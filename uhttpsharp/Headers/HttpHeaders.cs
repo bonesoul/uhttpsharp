580e6265fab542155119d7f75d6168f37429b054
Shani Elharrar <atnt123@bezeqint.net>
2014-01-21 14:27:50 +0200

    Moved HttpHeadersDebuggerDisplayProxy

--- uhttpsharp/Headers/HttpHeaders.cs
+++ uhttpsharp/Headers/HttpHeaders.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -55,51 +54,5 @@ namespace uhttpsharp.Headers
                 return _values.Count;
             }
         }
-    }
-
-    internal class HttpHeadersDebuggerProxy
-    {
-        private readonly IHttpHeaders _real;
-        
-        [DebuggerDisplay("{Value,nq}", Name = "{Key,nq}")]
-        internal class HttpHeader
-        {
-            private readonly KeyValuePair<string, string> _header;
-            public HttpHeader(KeyValuePair<string, string> header)
-            {
-                _header = header;
-            }
-
-            public string Value
-            {
-                get
-                {
-                    return _header.Value;
-                }
-            }
-
-            public string Key
-            {
-                get
-                {
-                    return _header.Key;
-                }
-            }
-        }
-
-        public HttpHeadersDebuggerProxy(IHttpHeaders real)
-        {
-            _real = real;
-        }
-
-        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
-        public HttpHeader[] Headers
-        {
-            get
-            {
-                return _real.Select(kvp => new HttpHeader(kvp)).ToArray();
-            }
-        }
-
     }
 }
